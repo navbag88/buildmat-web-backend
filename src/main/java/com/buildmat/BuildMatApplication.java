796efa920e2d399f81b9df4f2b8287f8c9097d42
Naveen Baghel <baghel.naveen8@gmail.com>
2026-04-14 11:57:32 +0530

    added comment

--- src/main/java/com/buildmat/BuildMatApplication.java
+++ src/main/java/com/buildmat/BuildMatApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling   // activates SessionService.cleanExpiredSessions cron
+@EnableScheduling   // activates SessionService.cleanExpiredSessions cron expression
 public class BuildMatApplication {
     public static void main(String[] args) {
         SpringApplication.run(BuildMatApplication.class, args);
